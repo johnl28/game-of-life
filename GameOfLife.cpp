4d1fd8bed490931f330aeb5c7550fcebaa55b2b8
johnl <54412955+johnl28@users.noreply.github.com>
2022-03-15 21:15:17 +0000

    Init

---
+++ GameOfLife.cpp
@@ -0,0 +1,137 @@
+#include "GameOfLife.h"
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+GridWorld::GridWorld(int newWidth, int newHeight) : height(newHeight), width(newWidth)
+{
+    srand(time(0)); // init seed for generating random cells
+    this->GenerateNextGeneration();
+}
+
+GridWorld::~GridWorld()
+{
+    this->__freeMemory();
+}
+
+void GridWorld::GenerateNextGeneration()
+{
+    this->__computeCellArray();
+    this->__generateOutputState();
+
+    // todo: function? (apply Single Responsibility Principle ?)
+    ++this->currentGeneration;
+}
+
+void GridWorld::__generateOutputState()
+{
+    if (!this->outputStateBuff) {
+        this->__initOutputState();
+    }
+
+    for (int y = 0; y < this->height; ++y)
+    {
+        for (int x = 0; x < this->width; ++x)
+        {
+            cell tmpCell = this->GetCellXY(x, y);
+            char stateOutputChar = tmpCell.alive ? ALIVE_CHAR : DEAD_CHAR;
+            this->SetOutputNewCharXY(x, y, stateOutputChar);
+        }
+    }
+}
+
+void GridWorld::__initOutputState()
+{
+    this->outputStateBuff = new char* [this->height];
+    for (int y = 0; y < this->height; ++y)
+    {
+        this->outputStateBuff[y] = new char[this->width + 1]{ 0 };
+
+        memset(this->outputStateBuff[y], DEAD_CHAR, this->width);
+    }
+}
+
+void GridWorld::__computeCellArray()
+{
+    if (!this->cellsArray)
+    {
+        this->__initCellsArray();
+        return;
+    }
+
+    auto cellPtr = this->cellsArray;
+    for (int i = 0; i < this->GetCellsCount(); ++i)
+    {
+        cellPtr->alive = this->__isCellAliveInNextGen(*cellPtr);
+        ++cellPtr;
+    }
+}
+
+
+void GridWorld::__initCellsArray()
+{
+
+    this->cellsArray = new cell[this->height * this->width];
+    for (int y = 0; y < this->height; ++y)
+    {
+        for (int x = 0; x < this->width; ++x)
+        {
+            bool isAlive = rand() % 2;
+            this->cellsArray[y * this->width + x] = cell{ x, y, isAlive };
+        }
+    }
+}
+
+bool GridWorld::__isCellAliveInNextGen(cell currCell)
+{
+    int globalX = currCell.x - 1;
+    int globalY = currCell.y - 1;
+    int neighboursAlive = 0;
+
+    for (int y = 0; y < 3; ++y)
+    {
+        int tmpY = globalY + y;
+        if (tmpY >= this->GetHeight()) {
+            break;
+        }
+
+        if (tmpY < 0) continue;
+
+        for (int x = 0; x < 3; ++x)
+        {
+            int tmpX = globalX + x;
+
+            if (tmpX >= this->GetWidth()) {
+                break;
+            }
+
+            if (tmpX < 0 || (tmpX == currCell.x && tmpY == currCell.y))
+                continue;
+
+            cell tmpCell = this->GetCellXY(tmpX, tmpY);
+            if (tmpCell.alive) {
+                ++neighboursAlive;
+            }
+        }
+    }
+
+    if (!currCell.alive && neighboursAlive == 3)
+        return true;
+
+    if (currCell.alive && (neighboursAlive == 3 || neighboursAlive == 2))
+        return true;
+
+    return false;
+}
+
+
+void GridWorld::__freeMemory()
+{
+    for (int i = 0; i < this->height; ++i)
+        delete[] this->outputStateBuff[i];
+
+    delete[] this->outputStateBuff;
+    delete[] this->cellsArray;
+
+
+}
---
+++ GameOfLife.h
@@ -0,0 +1,80 @@
+#ifndef GAME_OF_LIFE_H_
+#define GAME_OF_LIFE_H_
+
+#define ALIVE_CHAR 'O'
+#define DEAD_CHAR '-'
+
+
+struct cell {
+    int x = 0;
+    int y = 0;
+    bool alive = false;
+};
+
+class GridWorld {
+public:
+    GridWorld(int width, int height);
+    ~GridWorld();
+    void GenerateNextGeneration();
+
+
+    int GetHeight()
+    {
+        return this->height;
+    }
+
+    int GetWidth()
+    {
+        return this->width;
+    }
+
+    int GetCurrentGeneration()
+    {
+        return this->currentGeneration;
+    }
+
+    cell GetCellXY(int x, int y)
+    {
+        return this->cellsArray[y * this->width + x];
+    }
+
+    int GetCellsCount()
+    {
+        return this->width * this->height;
+    }
+
+    char* GetOutputRow(int index)
+    {
+        return this->outputStateBuff[index];
+    }
+
+    char GetOutputStateXY(int x, int y)
+    {
+        return this->outputStateBuff[y][x];
+    }
+
+    void SetOutputNewCharXY(int x, int y, char newChar)
+    {
+        this->outputStateBuff[y][x] = newChar;
+    }
+
+
+private:
+    void __freeMemory();
+    void __initCellsArray();
+    void __computeCellArray();
+
+    void __initOutputState();
+    void __generateOutputState();
+
+    bool __isCellAliveInNextGen(cell currCell);
+
+private:
+    int currentGeneration = 0;
+    int height = 0;
+    int width = 0;
+    cell* cellsArray = nullptr;
+    char** outputStateBuff = nullptr;
+};
+
+#endif
